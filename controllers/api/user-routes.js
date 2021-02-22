a02bac7715cd67319ede6b304817a50911bc1bea
quaylas <quayla@cxadvisors.com>
2021-02-22 00:13:52 -0700

    added initial styling with bulma

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -134,14 +134,14 @@ router.post('/signup', (req, res) => {
     });
 
 router.post('/logout', (req, res) => {
-if (req.session.loggedIn) {
-req.session.destroy(() => {
-    res.status(204).end();
-});
-}
-else {
-res.status(404).end();
-}
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+    });
+    }
+    else {
+        res.status(404).end();
+    }
 });
 
 router.put('/:id', (req, res) => {
