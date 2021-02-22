a41b90818b9c5bb40b6dc720b7a4713a9791fb73
quaylas <quayla@cxadvisors.com>
2021-02-21 22:41:26 -0700

    updated with functionality for comments

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const userRoutes  = require('./api/user-routes');
 const postRoutes = require('./api/post-routes');
+const commentRoutes =  require('./api/comment-routes');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
@@ -9,5 +10,6 @@ router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api/users', userRoutes);
 router.use('/api/posts', postRoutes);
+router.use('/api/comments', commentRoutes)
 
 module.exports = router;
